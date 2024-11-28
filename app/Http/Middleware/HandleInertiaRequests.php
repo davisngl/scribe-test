f3681fcb40c60eeb99358ef0fef9d70e7d2b0c2f
Davis Naglis <davisnaglis2@gmail.com>
2024-11-28 17:26:12 +0200

    add flash notifications in base response

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,6 +34,7 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'notifications' => $request->session()->get('success'),
         ];
     }
 }
